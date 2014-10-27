f2f8d5255e57eb5c4453f37afc6e7bb4a0f1aa48
unclenought <xh-chen@163.com>
2014-10-27 15:48:55 +0800

    2014年10月26号，第五期摇号结果

---
+++ enterprise_txt2mysql_new.py
@@ -0,0 +1,39 @@
+#@uncle nought
+# -*- coding:utf-8 -*-
+
+import csv
+import os
+import sys
+
+input_filename = sys.argv[1]
+output_filename = 'mysql_'+ input_filename
+input_file = open(input_filename)
+output_file = open(output_filename, 'w')
+input_line = input_file.readline()
+while input_line:
+	line = ''
+	if input_line.startswith('-'):
+		input_line = input_file.readline()
+		continue
+	elif input_line.startswith(' '):
+		line = input_line[1:]
+	else:
+		line = input_line
+
+	input_line = input_file.readline()
+	if input_line[1:2] >= '1' and input_line[1:2] <= '9':
+		print line
+		output_file.write(line)
+		continue;
+	elif input_line[0:1] == '-':
+		print line
+		output_file.write(line)
+		continue;
+	else:
+		line = line[0:-1] + input_line;
+		print line
+		output_file.write(line)
+		input_line = input_file.readline()
+
+input_file.close()
+output_file.close()
